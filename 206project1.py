552912520b2454f9cbf9e7ed31e4c0e6721fff2c
Sarah Jomaa <sljomaa@umich.edu>
2017-09-30 17:40:15 -0400

    modified 206project1.py, created results.csv, deleted Project1-master.zip

--- 206project1.py
+++ 206project1.py
@@ -1,4 +1,9 @@
 import os
+import datetime
+from datetime import datetime, timedelta
+import filecmp
+
+# Note: any comments are me rubber-ducky-ing to myself to help me figure out the code
 
 def getData(file):
 #Input: file name
@@ -6,20 +11,42 @@ def getData(file):
 #the keys will come from the first row in the data.
 
 #Note: The column headings will not change from the 
-#test cases below, but the the data itself will 
+#test cases below, but the data itself will 
 #change (contents and size) in the different test 
 #cases.
 
 	#Your code here:
-	pass
+	fh = open(file, "r")
+	fh = fh.read()
+	sfh = fh.split()
+	#print (fh)
+	#print ("\n **********", sfh)
+
+	lst = []
+	keys = sfh[0].split(",") #list of strings because I split it
+
+	for l in sfh[1:]:
+		fdict = {}
+		for w in l.split(","): #gives back student info on word-level
+			indx = l.split(",").index(w) #indexed with their position, returns a number
+			fdict[keys[indx]] = w 
+		lst.append(fdict)
+	return lst
+
+
 
 #Sort based on key/column
-def mySort(data,col):
+def mySort(data,col): #data is a list, col is a string "First", "Last" "Email"
 #Input: list of dictionaries
 #Output: Return a string of the form firstName lastName
-
+	new_d = {}
 	#Your code here:
-	pass
+	for d in data:
+		name = d["First"] + " " + d["Last"]
+		new_d[name] = d[col]
+	sorted_dict = sorted(new_d.keys(), key = lambda x: new_d[x])
+	return sorted_dict[0]
+ 
 
 #Create a histogram
 def classSizes(data):
@@ -29,7 +56,20 @@ def classSizes(data):
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
 
 	#Your code here:
-	pass
+	# print ("\n********HERE********", data)
+	counts = {}
+	# I'm trying to take the value of Class and make it a key in counts, where the value 
+	# is the number of times that value in Class occurs. So like, for loop. 
+	for d in data:
+		if d["Class"] not in counts:
+			counts[d["Class"]] = 1
+		else:
+			counts[d["Class"]] += 1
+	
+	class_sort = sorted(counts.items(), key = lambda x:x[1], reverse = True)
+	return class_sort
+
+
 
 
 
@@ -40,26 +80,63 @@ def findDay(a):
 # most often seen in the DOB
 
 	#Your code here:
-	pass
+	counts = {}
+
+	for d in a:
+		if (d["DOB"].split("/")[1]) not in counts:
+			counts[d["DOB"].split("/")[1]] = 1
+		else:
+			counts[d["DOB"].split("/")[1]] += 1
+	dom_sort = sorted(counts.keys(), key = lambda x: counts[x], reverse = True)
+	return int(dom_sort[0])
 
 
 # Find the average age (rounded) of the Students
 def findAge(a):
 # Input: list of dictionaries
-# Output: Return the day of month (1-31) that is the
-# most often seen in the DOB
+# Output: Return the average age of the students and round that age to the nearest 
+# integer. You will need to work with the DOB to find their current age.
+
 
 	#Your code here:
-	pass
+	 lst = []
+	 for d in a:
+	 	dob = d["DOB"]
+	 	dt = datetime.strptime(dob, '%m/%d/%Y')
+	 	dt2 = datetime.today()
+	 	timedelta = dt2 - dt
+	 	strTime = timedelta.__str__()
+	 	splitD = strTime.split()
+	 	d = (int(splitD[0]))
+	 	d = d/365
+	 	d = round(d)
+	 	lst.append(d)
+	
+	 aa = sum(lst)/len(lst)
+	 aa = round(aa)
+	 return aa
+	
 
-#Similar to mySort, but instead of returning single
-#Student, all of the sorted data is saved to a csv file.
-def mySortPrint(a,col,fileName):
-#Input: list of dictionaries, key to sort by and output file name
-#Output: None
+# Similar to mySort, but instead of returning single
+# Student, all of the sorted data is saved to a csv file.
+def mySortPrint(a,col,fileName): #a = list of dicts, col = Last, fileName = results.csv
+# Input: list of dictionaries, key to sort by and output file name
+# Output: None
 
 	#Your code here:
-	pass
+	# I need to take the list of dicts a, iterate through and grab
+	# just First, Last, and Email. 
+	
+	lst = []
+	for d in a:
+		lst.append((d['First'], d['Last'], d['Email']))
+	sorted_tup = sorted(lst, key = lambda x: x[1])
+
+	outfile = open(fileName, "w")
+	for student in sorted_tup: 
+		outfile.write("{},{},{},\n".format(*student))
+
+	outfile.close()
 
 
 
@@ -85,7 +162,7 @@ def main():
 	print("Read in Test data and store as a list of dictionaries")
 	data = getData('P1DataA.csv')
 	data2 = getData('P1DataB.csv')
-	total += test(type(data),type([]),35)
+	total += test(type(data),type([]),40)
 	print()
 	print("First student sorted by First name:")
 	total += test(mySort(data,'First'),'Abbot Le',15)
@@ -108,7 +185,7 @@ def main():
 	total += test(findDay(data2),26,10)
 	
 	print("\nThe average age is:")
-	total += test(findAge(data),39,10)
+	total += test(findAge(data),40,10)
 	total += test(findAge(data2),41,10)
 
 	print("\nSuccessful sort and print to file:")
